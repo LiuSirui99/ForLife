b1c2fc1933162baf4f90ad9396aeb1ae03cd2995
liusirui <liusirui1234@gmail.com>
2020-11-15 19:49:14 +0100

    Add popup window and password secrete

--- login_/LoginAndRegister.gd
+++ login_/LoginAndRegister.gd
@@ -1,32 +1,72 @@
 extends Control
-var DATABASE_FILE="res://user_information.json"
-var user_info = {
-	"UserName":"username",
-	"PassWord":"password"
-}
 
-
+var filelist = []
+var database = []
+var found = false
+var DATABASE_PATH="user://"
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
-	_on_Login_pressed()
+	_dir_contents(DATABASE_PATH)
+	_load_user()
+	$LoginRect/VBoxContainer/Password.secret = true
+	
+func _dir_contents(DATABASE_PATH):
+	var dir = Directory.new()
+	if dir.open(DATABASE_PATH) == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name!="":
+			if !dir.current_is_dir():
+				#print("Found file: " + file_name)
+				if file_name != ".DS_Store":
+					filelist.append(file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+
+func _load_user():
+	var file = File.new()
+	for f in filelist:
+		var DATABASE_FILE = DATABASE_PATH+f
+		if file.file_exists(DATABASE_FILE):
+			file.open(DATABASE_FILE, File.READ)
+			var cnt_users = file.get_as_text()
+			database.append(cnt_users)
+			file.close()
+		else:
+			_pop_no_file()
 
 func _on_Login_pressed():
-	if($NinePatchRect/VBoxContainer/Username.text != user_info.UserName || $NinePatchRect/VBoxContainer/Password.text != user_info.PassWord):
-		print("User doesn't exists ! Please check you input")
+	_load_user()
+	for d in database:
+		d = d.substr(2,d.length()-4)
+		var split = d.split(":")
+		var comma = to_json(split[1]).find(",")
+		#print(split[1].substr(1,comma-5)) # password
+		#print(split[2].substr(1,-1)) # username
+		if($LoginRect/VBoxContainer/Username.text != split[2].substr(1,-1) || $LoginRect/VBoxContainer/Password.text != split[1].substr(1,comma-5)):
+			found = false
+		else:
+			found = true
+			break
+	#print(found)
+	if found:
+		$LoginRect/Login_success.visible = true
+		$LoginRect/Login_success.get_ok().connect("pressed", self, "_change_scene")
 	else:
-		get_tree().change_scene("res://character appearance.tscn")
-		print("场景更换成功")
+		$LoginRect/Login_bad.visible = true
+		
 
+func _pop_no_file():
+	$LoginRect/Pop_no_file.visible = true
+	
+func _change_scene():
+	get_tree().change_scene("res://Main_page.tscn")
+	print("Change to character customized page")
 
-func _on_Register_pressed():
-	var file = File.new()
-	file.open(DATABASE_FILE,File.WRITE)
-	file.store_string(to_json(user_info))
-	file.close()
-	print(to_json(user_info))
-
-
-
+func _on_Login2Register_pressed():
+	get_tree().change_scene("res://Register.tscn")
 
 
 
